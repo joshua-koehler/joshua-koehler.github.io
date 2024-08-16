efff6192b9b4f4281715e9305eef934ce9ac5547
Joshua Koehler <joshua.koehler86@gmail.com>
2024-08-16 02:47:49 +0000

    correct closure time

--- assets/js/closed-on-sabbath.js
+++ assets/js/closed-on-sabbath.js
@@ -17,9 +17,7 @@ const sabbath_closed_html=`
 function close() {
   const d = new Date();
   // is Sabbath in local time or east coast US
-  const isSabbath = d.getDay() === 4 || (d.getUTCDay() === 1 && d.getUTCHours() < 5);
-  console.log(`isSabbath=${isSabbath}`);
-  //const isSabbath = d.getDay() === 0 || || (d.getUTCDay() === 1 && d.getUTCHours() < 5);
+  const isSabbath = d.getDay() === 0 || (d.getUTCDay() === 0 && d.getUTCHours() > 5) || (d.getUTCDay() === 1 && d.getUTCHours() < 5);
   if(isSabbath){
     document.documentElement.innerHTML = sabbath_closed_html;
     window.stop();
